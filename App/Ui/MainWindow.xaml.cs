4613203ef0aad7af91cf9cfd45a94b86a6ae6766
Trying to code <rea936405@gmail.com>
2026-08-24 01:17:39 -0700

    Create sound

--- App/Ui/MainWindow.xaml.cs
+++ App/Ui/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Media;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,7 +26,20 @@ namespace AutoSorter
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            createSound(@"Ui\Assets\Audio\SFX-Success1.mp3");
+        }
+
+        private void createSound(string soundFilePath)
+        {
+            //Debug::: if not making sound :::
+            //1. right click audio file in solution explorer;
+            //2. properties > Build action: Content;
+            //3. properties > Copy to output directory: always
+            Uri uri = new Uri(soundFilePath, UriKind.RelativeOrAbsolute);
+            MediaPlayer player = new MediaPlayer();
+
+            player.Open(uri);
+            player.Play();
         }
     }
 }
