a8cbb76cf9230765a4446394bae8f77da02b0fdc
Trying to code <rea936405@gmail.com>
2026-09-10 21:06:06 -0700

    (refactor) isolating startup behavior away from main window

--- App/Ui/MainWindow.xaml.cs
+++ App/Ui/MainWindow.xaml.cs
@@ -22,13 +22,11 @@ public partial class MainWindow : Window
     private readonly AppAPI _app;
     public MainWindow()
     {
-
         InitializeComponent();
 
         string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         _app = new AppAPI(path, DownloadFolder());
         
-
         Loaded += new RoutedEventHandler(Window_Loaded);
 
         UserSettings.GetSettings();
@@ -40,41 +38,10 @@ public partial class MainWindow : Window
         }
         System.Diagnostics.Debug.WriteLine(UserSettings.running);
 
-        RunOnStartup();
-    }
+        ActivateOnStartup.RunOnStartup();
+        if(ActivateOnStartup.DetectClose())
+            SendToTray();
 
-    void RunOnStartup()
-    {
-        //Send to registry
-        string RegistryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
-
-        RegistryKey regKey = Registry.CurrentUser.OpenSubKey(RegistryPath, true);
-        Debug.WriteLine("going to regKey: " + regKey);
-
-        string ExecutionPath = System.Windows.Forms.Application.ExecutablePath.ToString();
-        //ExecutionPath = @"C:\Users\Drago\source\repos\AutoSorter\bin\App\Debug\net10.0-windows\App.exe";
-
-        if (regKey.GetValue("AutoSorter") != ExecutionPath)
-        {
-            regKey.SetValue("AutoSorter", System.Windows.Forms.Application.ExecutablePath.ToString());
-            Debug.WriteLine("set regKey value: " + regKey.GetValue("AutoSorter"));
-        }
-        
-        //as a backup, make a shortcut in startup folder
-        string StartupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
-        CreateShortcut(StartupFolder, ExecutionPath);
-    }
-
-    private void CreateShortcut(string shortCutPath, string shortCutReferences)
-    {
-        WshShell shell = new WshShell();
-        string shortcutAddress = Path.Combine(shortCutPath, @"AutoSorter.lnk");
-        IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
-        shortcut.Description = "Shortcut to AutoSorter";
-        //shortcut.Hotkey = "Ctrl+Shift+A";
-        shortcut.TargetPath = shortCutReferences;
-        shortcut.WorkingDirectory = Path.GetDirectoryName(shortCutReferences);
-        shortcut.Save();
     }
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
---
+++ App/Ui/UiScripts/ActivateOnStartup.cs
@@ -0,0 +1,55 @@
+﻿using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+
+namespace App.Ui.UiScripts
+{
+    internal class ActivateOnStartup
+    {
+        public static bool DetectClose()
+        {
+            string[] FileArguments = Environment.GetCommandLineArgs();
+            if (FileArguments.Contains("--open-closed"))
+            {
+                int index = Array.IndexOf(FileArguments, "--open-closed");
+                if (FileArguments[index + 1] == "true")
+                    return true;
+            }
+            return false;
+        }
+        public static void RunOnStartup()
+        {
+            //Send to registry
+            string RegistryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey(RegistryPath, true);
+           // Debug.WriteLine("going to regKey: " + regKey);
+
+            string ExecutionPath = System.Windows.Forms.Application.ExecutablePath.ToString();
+            //ExecutionPath = @"C:\Users\Drago\source\repos\AutoSorter\bin\App\Debug\net10.0-windows\App.exe";
+
+            if (regKey.GetValue("AutoSorter") != ExecutionPath)
+            {
+                regKey.SetValue("AutoSorter", System.Windows.Forms.Application.ExecutablePath.ToString() + "--open - closed \"1\"");
+                Debug.WriteLine("set regKey value: " + regKey.GetValue("AutoSorter"));
+            }
+
+            //as a backup, make a shortcut in startup folder
+            string StartupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            CreateShortcut(StartupFolder, ExecutionPath);
+        }
+
+        public static void CreateShortcut(string shortCutPath, string shortCutReferences)
+        {
+            WshShell shell = new WshShell();
+            string shortcutAddress = Path.Combine(shortCutPath, @"AutoSorter.lnk");
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "Shortcut to AutoSorter";
+            shortcut.Arguments = "--open-closed \"1\"";
+            shortcut.TargetPath = shortCutReferences;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(shortCutReferences);
+            shortcut.Save();
+        }
+    }
+}
