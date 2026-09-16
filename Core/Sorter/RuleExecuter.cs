ba152357c165719b2d06e8885e2a970d83952d33
Trying to code <rea936405@gmail.com>
2026-09-16 15:35:07 -0700

    (add) Rule execution class functionality &&  Sort algorithm organization.

--- Core/Sorter/RuleExecuter.cs
+++ Core/Sorter/RuleExecuter.cs
@@ -1,8 +1,82 @@
 ﻿//Overview: Executes on all the rules made by SortAlgorithm
 
+using Core.FileSystem;
+using System.Diagnostics;
+
 namespace Core.Sorter
 {
     internal class RuleExecuter
     {
+        public static void ExecuteRule(Rule rule, string currentPath)
+        {
+            FileInfo fileInfo = new FileInfo(currentPath);
+
+            Debug.Assert(rule.ShouldMove(fileInfo, currentPath));
+        }
+
+        private static Rule? ShouldExecuteRule(List<Rule> allRules, string currentPath)
+        {
+            FileInfo? currentFileInfo = null;
+
+            for (int i = 0; i < allRules.Count; i++)
+            {
+                Rule rule = allRules[i];
+                if(rule.StartPath != currentPath) { continue; }
+
+                currentFileInfo ??= new FileInfo(currentPath);
+
+                if(rule.ShouldMove(currentFileInfo, currentPath))
+                {
+                    return rule;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Checks a folders' contents for any file that complies with the rule.
+        /// Executes on those files.
+        /// </summary>
+        /// <param name="rule">the rule to check</param>
+        /// <param name="folder">the path of the folder to check</param>
+        static void ExecuteRuleOnFolder(Rule rule, string folder)
+        {
+            const int fileLimit = 100;
+            IEnumerable<string> files = Directory.EnumerateFiles(folder).Take(fileLimit);
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                if (rule.ShouldMove(fileInfo, file))
+                {
+                    ExecuteRule(rule, file);
+                }
+            }
+        }
+
+        void LoopThroughRules(List<Rule>? rules)
+        {
+            if(rules == null) return;
+            //for rule in rules:
+            //    if ShouldExecuteRule(rule):
+            //        ExecuteRule(rule)
+        }
+
+        void OnFileMove(object sender, OnFileMoveEventArgs e)
+        {
+            LoopThroughRules(AllRules);
+        }
+
+        void OnFileCreated(object sender, OnFileMoveEventArgs e)
+        {
+            LoopThroughRules(AllRules);
+        }
+
+        static List<Rule>? AllRules {get; set;} = new List<Rule>();
+        static void OnRuleMade(object sender, OnRuleMadeEventArgs e)
+        {
+            if(AllRules == null)
+            ExecuteRuleOnFolder(e.rule, e.rule.StartPath);
+        }
     }
 }
--- Core/Sorter/SortAlgorithm.cs
+++ Core/Sorter/SortAlgorithm.cs
@@ -12,122 +12,131 @@ using Core.FileSystem;
 using System.Diagnostics;
 using static Core.FileSystem.AutoSorter;
 
-namespace Core.Sorter
+namespace Core.Sorter;
+
+/// <summary>
+/// Contains one paramater for a file, and what the parameter should or should not be.
+/// </summary>
+public struct Param()
 {
+    string FileProperty { get; set; } = "name";
+    string? PropertyContains { get; set; }
+    string? PropertyNotContains { get; set; }
+}
 
+/// <summary>
+/// Contains many paramaters, and a Method for detecting if those paramaters
+/// all match the paramater of a given file.
+/// </summary>
+public class Rule()
+{
+    List<Param> Paramaters { get; set; } = [];
+    public string StartPath { get; set; }
+    public string EndPath { get; set; }
     /// <summary>
-    /// Contains one paramater for a file, and what the parameter should or should not be.
+    /// the ammount of times the rule has been executed
     /// </summary>
-    struct Param()
+    public int Strength { get; set; } = 0; 
+
+    public bool ShouldMove(FileInfo fileInfo, string currentPath)
     {
-        string FileProperty { get; set; } = "name";
-        string? PropertyContains { get; set; }
-        string? PropertyNotContains { get; set; }
-    }
+        if (currentPath != StartPath) return false;
 
-    /// <summary>
-    /// Contains many paramaters, and a Method for detecting if those paramaters
-    /// all match the paramater of a given file.
-    /// </summary>
-    class Rule()
-    {
-        List<Param> Paramaters { get; set; } = [];
-        string StartPath { get; set; }
-        string EndPath { get; set; }
-
-        bool ShouldMove(FileInfo fileInfo, string currentPath)
-        {
-            if(currentPath != StartPath) return false;
-
-            return false;
-        }
-    }
-
-    /// <summary>
-    /// Class to get rid of useless data.
-    /// </summary>
-    class FilterData()
-    {
-        /// <summary>
-        /// Filters for only files that moved to and from the same path
-        /// as datapoint.
-        /// </summary>
-        /// <param name="data">all old data</param>
-        /// <param name="datapoint">just the newest datapoint</param>
-        /// <returns>The dataset minus extra datapoints.</returns>
-        public static object Filter(object data, object datapoint)
-        {
-            return new object();
-        }
-    }
-
-    internal class SortAlgorithm
-    {
-        /// <summary>
-        /// Tests a potential rule to see if it is worth prompting the user.
-        /// </summary>
-        /// <returns>How many files fit the rule.</returns>
-        static int TestRule(Rule rule)
-        {
-            return 0;
-        }
-
-        /// <summary>
-        /// Creates a potential rule based on the params given.
-        /// </summary>
-        /// <returns>Returns the created rule.</returns>
-        static Rule? GenerateRule(List<Param> parameters)
-        {
-            return null;
-        }
-
-        /// <summary>
-        /// Creates a list of every possible Rule that could be made based on the paramaters
-        /// given.
-        /// </summary>
-        /// <returns>The created list of Rules.</returns>
-        static List<Rule?> GenerateRules()
-        {
-            return [null];
-        }
-
-        static object oldData = new();
-
-        /// <summary>
-        /// Generates a list of all possible Rules given the old data and new file data.
-        /// </summary>
-        /// <returns>The best possible Rule, only if it is worth prompting the user.</returns>
-        static Rule? ManageRules(object oldData, OnFileMoveEventArgs newData)
-        {
-            //sort old data for only those files that match the start folder and end destination paths
-            object FilterFiles = FilterData.Filter(oldData, newData);
-
-            //generate a list of rules
-
-            //remove any rules if they already exist
-
-            //return best rule if applicable
-
-            return null;
-            oldData = newData;
-        }
-
-        //Called when, for example, a data entry is added to userAction.json
-        public void OnDataGained(object sender, OnFileMoveEventArgs e)
-        {
-            Debug.WriteLine("recieved data:");
-            int i = 0;
-            foreach (var datapoint in e.AllData)
-            {
-                i++;
-                Debug.WriteLine(i);
-                Debug.WriteLine("old path of datapoint:");
-                Debug.WriteLine(datapoint.OldPath);
-                Debug.WriteLine("new path of datapoint:");
-                Debug.WriteLine(datapoint.NewPath);
-            }
-            Debug.WriteLine(e);
-            //ManageRules(e);
-        }
+        return false;
     }
 }
+
+public class OnRuleMadeEventArgs : EventArgs
+{
+    required public Rule rule { get; set; }
+}
+
+
+/// <summary>
+/// Class to get rid of useless data.
+/// </summary>
+class FilterData()
+{
+    /// <summary>
+    /// Filters for only files that moved to and from the same path
+    /// as datapoint.
+    /// </summary>
+    /// <param name="data">all old data</param>
+    /// <param name="datapoint">just the newest datapoint</param>
+    /// <returns>The dataset minus extra datapoints.</returns>
+    public static object Filter(object data, object datapoint)
+    {
+        return new object();
+    }
+}
+
+internal class SortAlgorithm
+{
+    /// <summary>
+    /// Tests a potential rule to see if it is worth prompting the user.
+    /// </summary>
+    /// <returns>How many files fit the rule.</returns>
+    static int TestRule(Rule rule)
+    {
+        return 0;
+    }
+
+    /// <summary>
+    /// Creates a potential rule based on the params given.
+    /// </summary>
+    /// <returns>Returns the created rule.</returns>
+    static Rule? GenerateRule(List<Param> parameters)
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// Creates a list of every possible Rule that could be made based on the paramaters
+    /// given.
+    /// </summary>
+    /// <returns>The created list of Rules.</returns>
+    static List<Rule?> GenerateRules()
+    {
+        return [null];
+    }
+
+    static object oldData = new();
+
+    /// <summary>
+    /// Generates a list of all possible Rules given the old data and new file data.
+    /// </summary>
+    /// <returns>The best possible Rule, only if it is worth prompting the user.</returns>
+    static Rule? ManageRules(object oldData, OnFileMoveEventArgs newData)
+    {
+        //sort old data for only those files that match the start folder and end destination paths
+        object FilterFiles = FilterData.Filter(oldData, newData);
+
+        //generate a list of rules
+
+        //remove any rules if they already exist
+
+        //return best rule if applicable
+
+        return null;
+        oldData = newData;
+    }
+
+    //Called when, for example, a data entry is added to userAction.json
+    public void OnDataGained(object sender, OnFileMoveEventArgs e)
+    {
+        Debug.WriteLine("recieved data:");
+        int i = 0;
+        foreach (var datapoint in e.AllData)
+        {
+            i++;
+            Debug.WriteLine(i);
+            Debug.WriteLine("old path of datapoint:");
+            Debug.WriteLine(datapoint.OldPath);
+            Debug.WriteLine("new path of datapoint:");
+            Debug.WriteLine(datapoint.NewPath);
+        }
+        Debug.WriteLine(e);
+        //ManageRules(e);
+    }
+}
+
